f12870464af9af7c632a2d522a0b955abd020a6e
Soyeong4250 <th42500@naver.com>
2023-05-03 10:31:31 +0900

    [Feat] 감정 일기장 구현중

---
+++ src/pages/Edit.js
@@ -0,0 +1,40 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id : ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode : ", mode);
+
+  return (
+    <div>
+      <h1>Edit</h1>
+      <p>이곳은 일기 수정페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: "Soyeong" })}>
+        Query String 바꾸기
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        Home으로 가기
+      </button>
+
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로 가기
+      </button>
+    </div>
+  );
+};
+
+export default Edit;
---
+++ src/pages/Home.js
@@ -0,0 +1,66 @@
+import { useContext, useEffect, useState } from "react";
+import { DiaryStateContext } from "../App";
+
+import MyHeader from "./../components/MyHeader";
+import MyButton from "../components/MyButton";
+
+const Home = () => {
+  const diaryList = useContext(DiaryStateContext);
+
+  const [data, setData] = useState([]); // 가공된 데이터(날짜 넣기)
+  const [curDate, setCurDate] = useState(new Date());
+  console.log(curDate);
+
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  useEffect(() => {
+    const firstDay = new Date(
+      curDate.getFullYear(),
+      curDate.getMonth(),
+      1 // 1일
+    ).getTime();
+    console.log("firstDay = " + new Date(firstDay));
+
+    const lastDay = new Date(
+      curDate.getFullYear(),
+      curDate.getMonth() + 1,
+      0
+    ).getTime();
+    console.log("lastDay = " + new Date(lastDay));
+
+    setData(
+      diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay)
+    );
+  }, [diaryList, curDate]); // diaryList가 바뀌었을 때 동작하도록 하기 위해 diaryList도 배열에 넣어주어야 함
+
+  // 확인
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+
+  return (
+    <div>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      />
+      <h1>Home</h1>
+      <p>이곳은 홈 입니다.</p>
+    </div>
+  );
+};
+
+export default Home;
