4eea452c40c8be6bc186a661d4063e9811b51c8c
이소영 <th42500@naver.com>
2023-05-07 00:26:51 +0900

    [Feat] 감정 필터 추가 구현

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -4,7 +4,14 @@ const sortOptionList = [
   { value: "latest", name: "최신순" },
   { value: "oldest", name: "오래된 순" },
 ];
-// 정렬기능을 할 컴포넌트
+
+const filterOptionList = [
+  { value: "all", name: "전부 다" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "안좋은 감정만" },
+];
+
+// 필터기능을 할 컴포넌트
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
     <select value={value} onChange={(e) => onChange(e.target.value)}>
@@ -19,11 +26,23 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 
 const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState("latest");
+  const [filter, setFilter] = useState("all");
 
-  // 필터에 따른 정렬 다르게 하기
+  // (최신순, 오래된순)필터에 따른 정렬 다르게 하기
   const getProcessedDiaryList = () => {
     // sort를 사용하면 원본 배열 자체가 정렬되므로 원본 배열을 copy하여 정렬
     const copyList = JSON.parse(JSON.stringify(diaryList)); // 배열 => JSON 타입의 문자열로 변환 -> 배열로 복호화
+
+    const filterCallBack = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
+    // 감정필터를 이용한 필터링
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it));
 
     // 비교 함수
     const compare = (a, b) => {
@@ -34,7 +53,7 @@ const DiaryList = ({ diaryList }) => {
       }
     };
 
-    const sortedList = copyList.sort(compare);
+    const sortedList = filteredList.sort(compare);
     return sortedList; // 정렬된 리스트 반환
   };
 
@@ -45,8 +64,15 @@ const DiaryList = ({ diaryList }) => {
         onChange={setSortType}
         optionList={sortOptionList}
       />
+      <ControlMenu
+        value={filter}
+        onChange={setFilter}
+        optionList={filterOptionList}
+      />
       {getProcessedDiaryList().map((it) => (
-        <div key={it.id}>{it.content}</div>
+        <div key={it.id}>
+          {it.content} {it.emotion}
+        </div>
       ))}
     </div>
   );
