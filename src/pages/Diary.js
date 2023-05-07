1b0948a3811d1b1993b9f4d10a233261eff87657
이소영 <th42500@naver.com>
2023-05-08 01:18:53 +0900

    [Feat] 감정일기장 상세 페이지 구현중

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,15 +1,36 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+
+import { DiaryStateContext } from "../App.js";
 
 const Diary = () => {
   const { id } = useParams();
   console.log(id);
 
-  return (
-    <div>
-      <h1>Diary</h1>
-      <p>이곳은 일기 상세페이지 입니다.</p>
-    </div>
-  );
+  const diaryList = useContext(DiaryStateContext); // 저장된 일기 원본 데이터 가져오기
+
+  const navigate = useNavigate();
+
+  const [data, setData] = useState();
+  useEffect(() => {
+    const targetDiary = diaryList.find(
+      (it) => parseInt(it.id) === parseInt(id)
+    );
+    console.log(targetDiary);
+
+    // 존재하지 않는 일기의 접근 처리
+    if (targetDiary) {
+    } else {
+      alert("존재하지 않는 일기입니다.");
+      navigate("/", { repalce: true });
+    }
+  }, [id, diaryList]);
+
+  if (!data) {
+    return <div className="DiaryPage">로딩중입니다 :)</div>;
+  } else {
+    return <div className="DiaryPage"></div>;
+  }
 };
 
 export default Diary;
