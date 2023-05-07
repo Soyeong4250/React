82dfad7054e01f13530717e49907735e80295204
이소영 <th42500@naver.com>
2023-05-07 22:58:50 +0900

    [Feat] 일기장 작성 페이지 구현중

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,42 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+
+// 날짜 변환 함수
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10); // YYYY-MM-DDTHH:mm:ss.sssZ
+};
+
 const New = () => {
+  const navigate = useNavigate();
+
+  const [date, setDate] = useState(getStringDate(new Date())); // 오늘 날짜를 기본값으로
+  console.log(getStringDate(new Date())); // 날짜가 올바른 형식으로 출력되는지 확인
+
   return (
     <div>
+      <MyHeader
+        headText={"새 일기 쓰기"}
+        leftChild={
+          <MyButton text={"< 뒤로 가기"} onClick={() => navigate(-1)} />
+        }
+      />
       <h1>New</h1>
-      <p>이곳은 일기 작성페이지 입니다.</p>
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input_box">
+            <input
+              className="input_date"
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
