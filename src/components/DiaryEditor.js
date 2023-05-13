0de4dd22615d602886bd0397bf80c5d66b4f6e38
이소영 <th42500@naver.com>
2023-05-13 17:15:57 +0900

    [Feat] 일기 수정 페이지에 삭제 버튼 생성

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -28,7 +28,7 @@ const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
 
   // 작성완료를 위한 함수
-  const { onCreate, onEdit } = useContext(DiaryDispatchContext);
+  const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
   const handleSubmit = () => {
     if (content.length < 1) {
       contentRef.current.focus();
@@ -49,6 +49,14 @@ const DiaryEditor = ({ isEdit, originData }) => {
     navigate("/", { replace: true }); // 일기 작성하기를 뒤로가기로 다시 못돌아오게 하기 위해 replace 옵션을 true로 설정
   };
 
+  // 삭제
+  const handleRemove = () => {
+    if (window.confirm(`${originData.id}번 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(originData.id);
+      navigate("/", { replace: true });
+    }
+  };
+
   // 수정 페이지에서 활용
   useEffect(() => {
     if (isEdit) {
@@ -64,6 +72,17 @@ const DiaryEditor = ({ isEdit, originData }) => {
         headText={isEdit ? "일기 수정하기" : "새 일기 쓰기"}
         leftChild={
           <MyButton text={"< 뒤로 가기"} onClick={() => navigate(-1)} />
+        }
+        rightChild={
+          isEdit ? (
+            <MyButton
+              text={"삭제하기"}
+              type={"negative"}
+              onClick={handleRemove}
+            />
+          ) : (
+            ""
+          )
         }
       />
       <div>
